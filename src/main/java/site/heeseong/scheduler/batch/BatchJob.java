59eeba1a97e19f45b9945a0a26849372689cde9b
hshan <hhsung0120@naver.com>
2020-09-29 13:38:36 +0900

    스케쥴 락 테스트중

--- src/main/java/site/heeseong/scheduler/batch/BatchJob.java
+++ src/main/java/site/heeseong/scheduler/batch/BatchJob.java
@@ -1,17 +1,33 @@
 package site.heeseong.scheduler.batch;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class BatchJob {
+
+    private static final String ONE_MIN = "PT1M";
 
     //초 분 시 일 월 주 년
     @Scheduled(cron = "0/2 * * * * *")
-    private void scheduleTest() {
-        System.out.println("배치 시작");
+    @SchedulerLock(name = "scheduleTest1_schedul", lockAtMostFor = "PT2M", lockAtLeastFor = "PT1M")
+    public void scheduleTest1() {
+        System.out.println("배치 시작1");
+        try{
+            //throw new Exception();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(cron = "0/2 * * * * *")
+    public void scheduleTest2() {
+        System.out.println("배치 시작2");
     }
 
 }
